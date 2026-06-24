909c999a9773789a309cd3c924b3390108ed69fd
yangkaihu <yangkaihu@yeah.net>
2026-06-24 19:27:19 +0800

    first commit

---
+++ src/pages/page1.tsx
@@ -0,0 +1,9 @@
+const  Page1 =()=>{
+    return(
+        <>
+            <h2>this is page1</h2>
+            <link/>
+        </>
+    )
+}
+export default  Page1;
---
+++ src/pages/page2.tsx
@@ -0,0 +1,8 @@
+const  Page2 =()=>{
+    return(
+        <>
+            <h2>this is page2</h2>
+        </>
+    )
+}
+export default Page2;
